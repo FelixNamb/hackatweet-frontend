1696a77334e652b4929bef5d694115522a04f704
Felix <f.nambotin@orange.fr>
2024-07-18 20:14:44 +0200

    Rajout de redux pas global

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,24 @@
 import '../styles/globals.css';
 import Head from 'next/head';
 
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import {tweets} from '../reducers/tweets';
+import {users} from '../reducers/user';
+
+const store = configureStore({
+  reducer: { tweets,users },
+});
+
+
 function App({ Component, pageProps }) {
   return (
-    <>
+    <Provider store={store}>
       <Head>
         <title>Next.js App</title>
       </Head>
       <Component {...pageProps} />
-    </>
+    </Provider>
   );
 }
 
